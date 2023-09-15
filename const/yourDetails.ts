5debaf72be6c3e61b9c92c03c4dc032f3c8263c9
Jayboyd <85372143+Jayboyd@users.noreply.github.com>
2023-09-15 21:56:22 +0800

    Update yourDetails.ts

--- const/yourDetails.ts
+++ const/yourDetails.ts
@@ -1,5 +1,5 @@
 // Replace this with your contract address
-export const myEditionDropContractAddress: string = "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
+export const 0x22daEeE8F3aAf48E798387F1028D58B3D1b15cF0: string = "0xDC8017E1E20BFF80a49B0B92F719f00170013B4F";
 
 // Replace this with your token id
-export const tokenId: string = "0";
+export const 0: string = "0";
